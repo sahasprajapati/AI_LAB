1a6b82c1f9169502a1889c00f2218fba7174a9d5
Sahas Prajapati <b10prajapati@gmail.com>
2020-09-28 18:48:33 +0545

    Updates

---
+++ precache-manifest.7f055355d3cc37cf7ae5fda2a7b8defa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8b3383b3f66eb20ddd2f66285063e6",
+    "url": "/AI_LAB/index.html"
+  },
+  {
+    "revision": "da91af1f448c54fc444f",
+    "url": "/AI_LAB/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c358e3a8260a6e62dd3e",
+    "url": "/AI_LAB/static/js/2.ab901cb4.chunk.js"
+  },
+  {
+    "revision": "dfe93c012ee9c4e41ba7ac387341e16f",
+    "url": "/AI_LAB/static/js/2.ab901cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da91af1f448c54fc444f",
+    "url": "/AI_LAB/static/js/main.7304cd64.chunk.js"
+  },
+  {
+    "revision": "4f41a92582e81e25c78e",
+    "url": "/AI_LAB/static/js/runtime-main.334c7ec1.js"
+  }
+]);
