3ecc805d53e8760b5430adcb90d5d2616869fcf3
Sahas Prajapati <b10prajapati@gmail.com>
2020-10-18 11:49:25 +0545

    Updates

---
+++ precache-manifest.91d55d495112fa107abe18267bcde8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f57be32b58b43e05e6566abcfc5e683",
+    "url": "/AI_LAB/index.html"
+  },
+  {
+    "revision": "629394c2298412720431",
+    "url": "/AI_LAB/static/css/main.2ed3767f.chunk.css"
+  },
+  {
+    "revision": "293af78d684d44f9c5a8",
+    "url": "/AI_LAB/static/js/2.e197dc58.chunk.js"
+  },
+  {
+    "revision": "3fc774e6e737060b8d2cf94a43ab4223",
+    "url": "/AI_LAB/static/js/2.e197dc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629394c2298412720431",
+    "url": "/AI_LAB/static/js/main.c84e0714.chunk.js"
+  },
+  {
+    "revision": "4f41a92582e81e25c78e",
+    "url": "/AI_LAB/static/js/runtime-main.334c7ec1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AI_LAB/precache-manifest.fdcf3b62c307e66b191cda4919673d66.js"
+  "/AI_LAB/precache-manifest.91d55d495112fa107abe18267bcde8cc.js"
 );
 
 self.addEventListener('message', (event) => {
